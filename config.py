41c280e689511f3c654d3c3cbe0921c5c02fd98d
Lukacho2 <kartvelishvililukaa@gmail.com>
2024-07-01 12:57:27 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'vkjbsdvkbsdjjvjkjdkjssacascacnasscnacnacnoopsokOPKOP'
-UPLOAD_FOLDER = r'C:\Users\kartv\OneDrive\Desktop\Final Project\static'
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MPdatabase.db'
 admin_password = 'adminpass'
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 login_manager = LoginManager(app)
